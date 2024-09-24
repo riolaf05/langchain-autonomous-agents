be4dec0789404a50f02e2290f2db5f2d1bf14b89
Rosario Laface <lafacerosario@gmail.com>
2024-09-24 15:17:02 +0200

    sql agent

--- agents/sql_agent/sql_agent.py
+++ agents/sql_agent/sql_agent.py
@@ -1,11 +1,11 @@
-from langchain_openai import OpenAI
-from langchain import hub
-from langchain.agents import AgentExecutor, create_react_agent
-from langchain_groq import ChatGroq
-from langchain.utilities import SQLDatabase
-from langchain.agents import create_sql_agent
-from langchain.agents.agent_toolkits import SQLDatabaseToolkit
-from langchain.agents.agent_types import AgentType
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import json
+import sqlite3
+load_dotenv(override=True)
 from utils.rabbitmq import RabbitMQClient
 import logging
 from dotenv import load_dotenv
@@ -18,20 +18,24 @@ QUEUE_NAME=os.getenv('RABBITMQ_QUEUE_NAME')
 RABBITMQ_URL=os.getenv('RABBITMQ_URL')
 GROQ_API_KEY=os.getenv('GROQ_API_KEY')
 
-prompt_react = hub.pull("hwchase17/react")
-tools = [get_today_date_tool, web_search_tool]
-db = SQLDatabase.from_uri("sqlite:///test.db")
+conn = sqlite3.connect('example.db')
+# llm = ChatGroq(model_name="llama3-70b-8192", groq_api_key=GROQ_API_KEY, temperature=0)
+model = ChatOpenAI(temperature=0)
 
-# Initialize ChatGroq model for language understanding
-llm = ChatGroq(model_name="llama3-70b-8192", groq_api_key=GROQ_API_KEY, temperature=0)
+# Define your desired data structure.
+class Observation(BaseModel):
+    title: str = Field(description="the title of the observation")
+    description: str = Field(description="the detailed description of the observation")
 
-# Create ReAct agent
-agent_executor = create_sql_agent(
-    llm=llm,
-    toolkit=SQLDatabaseToolkit(db=db, llm=llm),
-    verbose=True,
-    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+# Set up a parser + inject instructions into the prompt template.
+parser = JsonOutputParser(pydantic_object=Observation)
+
+prompt = PromptTemplate(
+    template="Rispondi alla domanda.\n{format_instructions}\n{query}\n",
+    input_variables=["query"],
+    partial_variables={"format_instructions": parser.get_format_instructions()},
 )
+chain = prompt | model | parser
 
 rabbit_client=RabbitMQClient(
     host=RABBITMQ_URL
@@ -41,8 +45,9 @@ rabbit_client.declare_queue(QUEUE_NAME)
 def callback(body):
         print(f" [x] Received {body}")
         try:
-            response = agent_executor.run({"input": body['story']})
-            rabbit_client.send_message('', QUEUE_NAME, {'story': response})
+            data = chain.invoke({"query": body['description']})
+            conn.execute("INSERT INTO example_table (title, description) VALUES (?, ?)", 
+                (data["title"], data["description"]))
             print('saved!')
         except Exception as e:
             print(e)
